61cb73be6b70973122c2a279fb4492e0adbfd0ff
titochainaluzzenaida-ai <titochainaluzzenaida@gmail.com>
2026-09-16 12:02:45 -0500

    Create 6936. Programa de amor.cpp

---
+++ 6936. Programa de amor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n,m,a;
+    cin>>n>>m>>a;
+    if (n==m==a)
+    {
+        cout<<"ELLA TE AMA";
+    }
+    else
+    {
+        cout<<"ELLA NO TE AMA";
+    }
+    return 0;
+}
+
