a8534a62b0cccd7467f9df4b6d2d53e12d0eb728
titochainaluzzenaida-ai <titochainaluzzenaida@gmail.com>
2026-09-16 12:00:47 -0500

    Create 8375. Las cemitas programadoras.cpp

---
+++ 8375. Las cemitas programadoras.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a,b,m;
+    cin>>a>>b;
+    m=a-b;
+    if (a>b)
+    {
+    cout<<"El negocio si va jalar "<<m;
+    }
+    else
+    {
+        cout<<"dejalo ya esta muerto "<<m;
+    }
+    return 0;
+}
+
