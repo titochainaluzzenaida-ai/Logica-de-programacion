097a3a3e5054d4cd733f6bc91bca0767f55b3177
titochainaluzzenaida-ai <titochainaluzzenaida@gmail.com>
2026-09-16 11:59:20 -0500

    Create 11318. Toma de decisiones.cpp

---
+++ 11318. Toma de decisiones.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,m,n,par,impar;
+cin>>a>>b;
+if (a+b==5)
+{
+b+=3;
+m=(2*a)+b;
+cout<<m;
+}
+else
+{
+    --a;
+    n=(7*a)+b;
+    if (n%2==0)
+    {
+    par=a-b;
+   
+    cout<<par;
+    }
+    else
+    {
+    impar=a*b;
+    cout<<impar;
+    }
+}
+return 0;
+}
+
